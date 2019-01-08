941e25a38b4e10943cf61d0a87ce828e27cd8698
_2Binary <3264722570@qq.com>
2019-01-08 12:00:21 +0800

    first step

---
+++ Back/Back/src/com/firstStep/SayHello.java
@@ -0,0 +1,9 @@
+package com.firstStep;
+
+public class SayHello {
+	public static void main(String[] args) {
+		//在这里用分支名声明声明一个值为自己名字的字符串
+		String jiandev = "jianzhirui";
+		System.out.println(jiandev/*+*/);
+	}
+}
