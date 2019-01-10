46efb53ecf4271d4bcd396e7cf2847ea7f722376
yanlei <yan1ei@126.com>
2019-01-10 12:07:47 +0800

    Add var

--- Back/Back/src/com/firstStep/SayHello.java
+++ Back/Back/src/com/firstStep/SayHello.java
@@ -4,6 +4,7 @@ public class SayHello {
 	public static void main(String[] args) {
 		//在这里用分支名声明声明一个值为自己名字的字符串
 		String jiandev = "jianzhirui";
+		String yandev = "YanLei";
 		System.out.println(jiandev/*+*/);
 	}
 }
